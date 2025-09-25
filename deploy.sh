806311211928458b342bad7d9c9707029e4a303c
0xfps <anthony.nnaemeka.umeh@gmail.com>
2025-09-25 01:11:46 +0100

    feat: create deployment script file.

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+npx hardhat run script/deploy.ts --network arbitrumSepolia
+npx hardhat run script/deploy.ts --network avalancheFuji
+npx hardhat run script/deploy.ts --network baseSepolia
+npx hardhat run script/deploy.ts --network bscTestnet
+npx hardhat run script/deploy.ts --network optimismSepolia
+npx hardhat run script/deploy.ts --network polygonAmoy
+npx hardhat run script/deploy.ts --network sepolia
